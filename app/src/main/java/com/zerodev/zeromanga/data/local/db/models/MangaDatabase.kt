ed22b0ccbf465dc42af0638bfe79c327a8e3b169
Julio olivares <julioolivares90@hotmail.com>
2022-05-29 20:36:50 -0600

    reparado error de migracion

--- app/src/main/java/com/zerodev/zeromanga/data/local/db/models/MangaDatabase.kt
+++ app/src/main/java/com/zerodev/zeromanga/data/local/db/models/MangaDatabase.kt
@@ -1,13 +1,15 @@
 package com.zerodev.zeromanga.data.local.db.models
 
 import android.content.Context
-import androidx.navigation.dynamicfeatures.Constants
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.zerodev.zeromanga.data.local.db.MangaCacheDao
 import com.zerodev.zeromanga.data.local.db.MangaFavDao
 import com.zerodev.zeromanga.utlities.constantes.DATABASE_NAME
+
 
 @Database(
     entities = [MangaFav::class,MangaCache::class],
@@ -19,6 +21,13 @@ abstract class MangaDatabase : RoomDatabase() {
     abstract fun getMangaCacheDao():MangaCacheDao
 
     companion object {
+        //para cuando se actualiza la Base
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE TABLE mangaCache (id INTEGER not null primary key AUTOINCREMENT ,title text not null,mangaUrl text not null,type text not null,demography text not null , score text not null,mangaImagen text not null)")
+            }
+        }
+
         @Volatile
         private var INSTANCE : MangaDatabase? = null
 
@@ -28,7 +37,7 @@ abstract class MangaDatabase : RoomDatabase() {
                     context.applicationContext,
                     MangaDatabase::class.java,
                     DATABASE_NAME
-                ).build()
+                ).addMigrations(MIGRATION_1_2).build()
                 INSTANCE = instance
                 instance
             }
